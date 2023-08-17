b6bd2c10d0ce91f1b7dec7e35ddbfd4b70fb85e2
Ryota KITA <snptkdn@snptkdn.local>
2023-08-18 02:49:23 +0900

    なんか色々コミット

--- go.mod
+++ go.mod
@@ -3,16 +3,21 @@ module pomodoro
 go 1.20
 
 require (
-	github.com/mattn/go-runewidth v0.0.14
+	github.com/gen2brain/beeep v0.0.0-20230812223410-3e1549ef0811
 	github.com/rivo/tview v0.0.0-20230814110005-ccc2c8119703
 )
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.6.0 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,42 +4,113 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/gen2brain/beeep"
 	"github.com/rivo/tview"
 )
 
 const refreshInterval = 1000 * time.Millisecond
 
 var (
-	view  *tview.Modal
-	app   *tview.Application
-	timer time.Time
+	view            *tview.Modal
+	app             *tview.Application
+	timer           time.Time
+	isPause         = false
+	pomodoroEndTime = 5 * time.Second
+	breakEndTime    = 10 * time.Second
+	lunchEndTime    = 20 * time.Second
+	pomodoroCount   = 0
+	isBreak         = false
 )
 
 func timeString() string {
 	return fmt.Sprintf(timer.Format("15:04:05"))
 }
 
+func statusString() string {
+	if isPause {
+    return "PAUSE"
+	} else if isBreak && pomodoroCount == 3 {
+		return "LUNCH"
+	} else if isBreak && pomodoroCount != 4 {
+    return fmt.Sprintf("BREAK:%d", pomodoroCount+1)
+	} else {
+    return fmt.Sprintf("FOCUS:%d", pomodoroCount+1)
+	}
+}
+
 func tick() {
 	timer = timer.Add(time.Second)
 }
 
-func updateTime() {
+func beep(status string) {
+	err := beeep.Notify("Gomodoro", fmt.Sprintf("%s time is ended!", status), "nothing")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func updateTime(ticker time.Ticker, stopTimer chan int) {
 	for {
-		time.Sleep(refreshInterval)
-		tick()
-		app.QueueUpdateDraw(func() {
-      if 
-			view.SetText(timeString())
-		})
+		select {
+		case <-ticker.C:
+			time.Sleep(refreshInterval)
+			tick()
+			if !isBreak && timer.Sub(time.Time{}) == pomodoroEndTime {
+				beep("Focus")
+			}
+      if isBreak && pomodoroCount != 4 && timer.Sub(time.Time{}) == breakEndTime {
+        beep("Break")
+      }
+      if isBreak && pomodoroCount == 4 && timer.Sub(time.Time{}) == lunchEndTime {
+        beep("Lunch")
+      }
+			app.QueueUpdateDraw(func() {
+				view.SetText(fmt.Sprintf("%s: %s", statusString(), timeString()))
+			})
+		case <-stopTimer:
+			fmt.Println("stopped!")
+			return
+		}
 	}
 }
 
 func main() {
 	timer = time.Time{}
+	ticker := time.NewTicker(1000 * time.Millisecond)
+	chStop := make(chan int, 1)
+	go updateTime(*ticker, chStop)
 	app = tview.NewApplication()
-  view = tview.NewModal().SetText(timeString()).AddButtons([]string{"▶︎","■"})
+	view = tview.
+		NewModal().
+		SetText(fmt.Sprintf("%s: %s", statusString(), timeString())).
+		AddButtons([]string{"Next", "Pause", "Reset"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			if buttonLabel == "Pause" && !isPause {
+				ticker.Stop()
+				chStop <- 0
+				close(chStop)
+				isPause = true
+			} else if buttonLabel == "Pause" && isPause {
+				ticker = time.NewTicker(1000 * time.Millisecond)
+				chStop = make(chan int, 1)
+				go updateTime(*ticker, chStop)
+				isPause = false
+			} else if buttonLabel == "Reset" {
+				timer = time.Time{}
+			} else if buttonLabel == "Next" {
+        timer = time.Time{}
+        if !isBreak {
+          isBreak = true
+        } else {
+          isBreak = false
+          pomodoroCount++
+          if pomodoroCount == 4 {
+            pomodoroCount = 0
+          }
+        }
+      }
+		})
 
-	go updateTime()
 	if err := app.SetRoot(view, false).Run(); err != nil {
 		panic(err)
 	}
